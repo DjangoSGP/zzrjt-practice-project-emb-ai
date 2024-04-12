cdfb7a866c6f3b13d7222f0eba60feaf7ef6f825
Santiago <santigomezp13@gmil.com>
2024-04-12 16:35:32 -0400

    last lab

---
+++ test_sentiment_analysis.py
@@ -0,0 +1,9 @@
+from SentimentalAnalysis.sentiment_analysis import sentiment_analyzer
+import unittest
+
+class TestSentimentsAalyzer(unittest.TestCase):
+    def test_sentiment_analyzer(self):
+        self.assertEqual(sentiment_analyzer('I love working with Python')['label'], 'SENT_POSITIVE')
+        self.assertEqual(sentiment_analyzer('I hate working with Pyhton')['label'], 'SENT_NEGATIVE')
+        self.assertEqual(sentiment_analyzer('I am neutral on Python')['label'], 'SENT_NEUTRAL')
+unittest.main()
